8732ba1edb4e2d62eb7f2b1f92da014be5b4cb94
Ramon Asuncion <ramon@b1t.dev>
2025-03-09 06:07:28 -0400

    Read response from webpage.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 #include <string>
 #include <iostream>
 
-bool detect_sql_injection(const std::string& response)
+// reference: https://curl.se/libcurl/c/libcurl-tutorial.html
+
+
+/**
+ * Implement:
+ * Port scanning.
+ */
+
+size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-  return false;
+  size_t total_size = size * nmemb;
+  ((std::string*)userp)->append((char*)contents, total_size);
+  return total_size;
 }
 
 bool is_url_valid(const std::string& url)
@@ -33,8 +43,24 @@ int main()
 
   if (!curl) {
     std::cerr << "Could not initialize curl" << "\n";
+    curl_global_cleanup();
     return 1;
   }
+
+  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
+
+  res = curl_easy_perform(curl);
+
+  if (res != CURLE_OK) {
+    std::cerr << "Curl failed: " << curl_easy_strerror(res) << "\n";
+  }
+
+  std::cout << read_buffer << "\n";
+
+  curl_easy_cleanup(curl);
+  curl_global_cleanup();
 
   printf("Exiting...!\n");
   return 0;
