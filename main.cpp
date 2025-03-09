f3062a92a9f5ed9b09fdafc2b1884a007eb1759e
Ramon Asuncion <ramon@b1t.dev>
2025-03-09 06:28:13 -0400

    Display menu.
    Commented out the libcurl code.

--- main.cpp
+++ main.cpp
@@ -22,46 +22,84 @@ bool is_url_valid(const std::string& url)
   return (url.substr(0, 7) == "http://") || (url.substr(0, 8) == "https://");
 }
 
+void display_menu()
+{
+  std::cout << "Vigilis tool:\n";
+  std::cout << "1. Port Scanning\n";
+  std::cout << "2. Exit\n";
+  std::cout << "\n" << std::string(10, '-') << "\n";
+}
+
 int main()
 {
-  std::string url;
-  std::cout << "Enter a URL: ";
-  std::getline(std::cin, url);
+  int choice = 0;
 
-  if (!is_url_valid(url)) {
-    std::cerr << "Invalid URL: " << url << "\n";
-    return 1;
-  }
+  do {
+    display_menu();
 
-  CURL *curl;
-  CURLcode res;
-  std::string read_buffer;
+    std::cin >> choice;
 
-  // init
-  curl_global_init(CURL_GLOBAL_DEFAULT);
-  curl = curl_easy_init();
+    // reference: https://isocpp.org/wiki/faq/input-output#istream-and-ignore
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid choice\n";
+      continue;
+    }
 
-  if (!curl) {
-    std::cerr << "Could not initialize curl" << "\n";
+    switch (choice) {
+      case 1:
+        std::cout << "Picked option 1\n";
+        break;
+      case 2:
+        std::cout << "Exiting...\n";
+        break;
+      default:
+        std::cout << "Invalid choice" << "\n";
+    }
+  } while (choice != 2);
+
+
+  #ifdef DEBUG
+    std::string url;
+    std::cout << "Enter a URL: ";
+    std::getline(std::cin, url);
+
+    if (!is_url_valid(url)) {
+      std::cerr << "Invalid URL: " << url << "\n";
+      return 1;
+    }
+
+    CURL *curl;
+    CURLcode res;
+    std::string read_buffer;
+
+    // init
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (!curl) {
+      std::cerr << "Could not initialize curl" << "\n";
+      curl_global_cleanup();
+      return 1;
+    }
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
+
+    res = curl_easy_perform(curl);
+
+    if (res != CURLE_OK) {
+      std::cerr << "Curl failed: " << curl_easy_strerror(res) << "\n";
+    }
+
+    std::cout << read_buffer << "\n";
+
+    curl_easy_cleanup(curl);
+
     curl_global_cleanup();
-    return 1;
-  }
+  #endif
 
-  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
-
-  res = curl_easy_perform(curl);
-
-  if (res != CURLE_OK) {
-    std::cerr << "Curl failed: " << curl_easy_strerror(res) << "\n";
-  }
-
-  std::cout << read_buffer << "\n";
-
-  curl_easy_cleanup(curl);
-  curl_global_cleanup();
-
-  printf("Exiting...!\n");
   return 0;
 }
